e3a376ad2c68c48a838193727f399ad98fffffa5
kyrie-dark-room <465411023@qq.com>
2022-06-15 23:51:44 +0800

    完成错误处理类：新增错误码获取和错误码信息获取公有函数

--- DR_WINDOWS_APPLICATION/sourcecode/Windows_Core_Programming/Windows_Core_Programming/CHYErrorHandler.cpp
+++ DR_WINDOWS_APPLICATION/sourcecode/Windows_Core_Programming/Windows_Core_Programming/CHYErrorHandler.cpp
@@ -1,1 +1,63 @@
 #include "CHYErrorHandler.h"
+
+namespace HY_Windows_ErrorHandle
+{
+	CHYErrorHandler::CHYErrorHandler()
+	{
+
+	}
+
+	CHYErrorHandler::~CHYErrorHandler()
+	{
+
+	}
+
+	DWORD CHYErrorHandler::GetLastError()
+	{
+		return ::GetLastError();
+	}
+
+	std::wstring CHYErrorHandler::GetLastErrorMsg()
+	{
+		std::wstring sLastErrorMsg;
+
+		// Get the error code
+		DWORD dwError = GetLastError();
+
+		// Buffer that gets the error message string
+		HLOCAL hlocal = NULL;
+
+		// Use the default system locale since we look for windows messages
+		// Note: this MAKELANGID combination has a value of 0
+		DWORD systemLocale = MAKELANGID(LANG_NEUTRAL, SUBLANG_NEUTRAL);
+
+		// Get the error code's textual description
+		BOOL bOK = ::FormatMessage(
+						FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_ALLOCATE_BUFFER,
+						NULL, dwError, systemLocale, (PTSTR)&hlocal, 0, NULL);
+		if (!bOK)
+		{
+			// Is it a network-related error?
+			HMODULE hDll = LoadLibraryEx(TEXT("netmsg.dll"), NULL, DONT_RESOLVE_DLL_REFERENCES);
+			if (hDll != NULL)
+			{
+				bOK = ::FormatMessage(
+					FORMAT_MESSAGE_FROM_HMODULE | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_ALLOCATE_BUFFER,
+					hDll, dwError, systemLocale, (PTSTR)&hlocal, 0, NULL);
+				FreeLibrary(hDll);
+			}
+
+		}
+
+		if (bOK && (hlocal != NULL))
+		{
+			sLastErrorMsg = static_cast<std::wstring>((PCTSTR)LocalLock(hlocal));
+		}
+		else
+		{
+			sLastErrorMsg = TEXT("No text found for the last error number.");
+		}
+
+		return sLastErrorMsg;
+	}
+}
--- DR_WINDOWS_APPLICATION/sourcecode/Windows_Core_Programming/Windows_Core_Programming/CHYErrorHandler.h
+++ DR_WINDOWS_APPLICATION/sourcecode/Windows_Core_Programming/Windows_Core_Programming/CHYErrorHandler.h
@@ -52,10 +52,66 @@
 * 
 *	  可以看出，每个错误都有三种表示：一个消息ID（一个可在源代码中使用的宏，用于与GetLastError的返回值进行比较）、消息文本（描述错误的英文文本）和一个编号
 *  （应该避免使用此编号，尽量使用消息ID）。
+* 
+* 其它知识：
+*	  1）在Microsoft Visual Studio中，Microsoft的调试器支持一个很有用的功能--我们可以配置Watch（监视）窗口，让它始终显式线程的上一个错误代码和错误的文本描述。具体的
+*   做法是：在Watch窗口中选择一行，然后输入$err,hr。
+*	  2）Visual Studio还搭载了一个很小的实用程序，名为Error Lookup。利用它，可以将错误代码转换为相应的文本描述。
+*	  3）Windows提供了一个函数，可以将错误代码转换为相应的文本描述。次函数名为FormatMessage，如下所示：
+*			DWORD FormatMessage(
+*				DWORD		dwFlags,
+*				LPCVOID		pSource,
+*				DWORD		dwMessageId,
+*				DWORD		dwLanguageId,
+*				PTSTR		pszBuffer,
+*				DWORD		nSize,
+*				va_list		*Arguments);
+*	  4）为了指出错误，只需设置线程的上一个错误代码，然后令自己的函数返回FALSE，INVALID_HANDLE_VALUE、NULL或者其他合适的值。为了设置线程的上一个错误代码，只需要
+*   调用以下函数，并传递我们认为合适的任何32位值：
+*			VOID SetLastError(DWORD dwErrCode);
+*     5）我们尽量使用WinError.h中出现的代码--只要代码能很好地反应我们想报告的错误。如果WinError.h中的任何一个代码都不能准确反应一个错误，就可以创建自己的代码。错误
+*   代码是一个32位数，由下表描述的几个不同的字段组成
+*        ——————————————————————————————————————————————————
+*			位	|		31-30		|		29		|		28		|		27-16		|		15-0
+*		 ——————————————————————————————————————————————————
+*		  内容  | 严重性			| Microsoft/客户| 保留			| Facility代码		| 异常代码
+*		 ——————————————————————————————————————————————————
+*		  含义  | 0 = 成功			| 0 = Microsoft | 必须为0		| 前256个值由		| Microsoft/客户
+*				| 1 = 信息（提示）  | 定义的代码    |				| Microsoft保留		| 定义的代码
+*				| 2 = 警告			| 1 = 客户定义  |				|					|
+*				| 3 = 错误			| 的代码		|				|					|
+*		 ——————————————————————————————————————————————————
+*        唯一需要注意的重要字段是第29位。Microsoft承诺，在它所生成的所有错误代码中，此位将始终为0。但是，如果要创建我们自己的错误代码，就必须在此位放入一个1。通过
+*      这种方式，可以保证我们的错误码绝对不会与Microsoft现在和将来定义的错误代码冲突。注意，Facility字段非常大，足以容纳4096个可能的值。其中，前256个值是为Microsoft
+*      保留的，其余的值可由我们自己的应用程序来定义。
+*	  6）每个DLL(或.exe)都可以有自己的一套错误代码。我们也可以向自己的模块添加错误代码，具体做法是使用Message Compiler（MC.exe）来创建一个消息资源并将其添加到DLL(或.exe)模块中。
 *     
 */
 
-class CHYErrorHandler
+#include <Windows.h>
+#include <string>
+
+namespace HY_Windows_ErrorHandle
 {
-};
+	class CHYErrorHandler
+	{
+	public:
+		CHYErrorHandler();
+		virtual ~CHYErrorHandler();
+		CHYErrorHandler(const CHYErrorHandler&) {};
+		CHYErrorHandler& operator=(const CHYErrorHandler&) {};
+		CHYErrorHandler(CHYErrorHandler&& s) {};
+
+		// 获取错误码
+		DWORD GetLastError();
+		// 获取错误信息
+		std::wstring GetLastErrorMsg();
+
+	protected:
+
+	private:
+
+	};
+
+}
 
